3511c4e33c2db6ce6cd9f0fdbf542c41cb2afd44
yuki5664 <monkey.yebisu.7777@gmail.com>
2020-03-31 11:43:35 +0900

    add new画面

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,19 @@
 class PostsController < ApplicationController
+
+  def new
+    @post = Post.new # フォーム用の空のインスタンスを生成する。
+  end
+
+  def create
+    @post = Post.new(post_params) # ストロングパラメータを引数に
+    @post.save # saveをしてデータベースに保存する。
+    redirect_to @post # showページにリダイレクト
+  end
+
+  private
+
+  def post_params # ストロングパラメータを定義する
+    params.require(:post).permit(:caption)
+  end
+
 end
