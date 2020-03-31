c3cb2fa474151d3d15496889787b52871f77077e
yuki5664 <monkey.yebisu.7777@gmail.com>
2020-03-31 12:38:45 +0900

    add flash機能

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,7 +7,7 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params) # ストロングパラメータを引数に
     @post.save # saveをしてデータベースに保存する。
-    redirect_to @post # showページにリダイレクト
+    redirect_to @post, notice: '投稿を保存しました' # showページにリダイレクト
   end
   
     def show
