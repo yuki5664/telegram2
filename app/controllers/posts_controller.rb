b0d8f5a9d06f486aff11730fea0c416bb0eae58e
yuki5664 <monkey.yebisu.7777@gmail.com>
2020-04-03 11:32:21 +0900

    add パーシャル機能

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [ :show, :edit, :update, :destroy]
 
   def index
     @posts = Post.order(created_at: :desc)
@@ -18,15 +19,12 @@ class PostsController < ApplicationController
   end
   
     def show
-      @post = Post.find(params[:id])
     end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
     if @post.update(post_params)
       redirect_to @post, notice: "投稿を更新しました。"
     else
@@ -35,7 +33,6 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    @post = Post.find(params[:id])
     @post.destroy
     redirect_to posts_path, notice: "投稿を削除しました。"
   end
@@ -46,4 +43,7 @@ class PostsController < ApplicationController
     params.require(:post).permit(:caption, :image)
   end
 
+  def set_post
+    @post = Post.find(params[:id])
+  end
 end
